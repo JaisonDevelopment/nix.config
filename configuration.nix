2412e1bdef3aae3f2c9b2c120ebe558019704071
Jaison Palacio <95976457+JaisonDevelopment@users.noreply.github.com>
2022-10-19 16:01:55 -0500

    archivos subidos

---
+++ configuration.nix
@@ -0,0 +1,111 @@
+{ config, pkgs, lib, .. };
+
+{
+  imports = 
+    [
+	./flake.nix
+	./hardware-configuration.nix
+	./module.nix
+    ];
+
+  # systemd-boot 
+  boot.loader.systemd-boot.enable = true; 
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.consoleMode = "2"; 
+  
+  # Borrar /tmp durante el arranque
+  boot.cleanTmpDir = true; 
+
+  # Usar el kernel más reciente 
+  nixpkgs.config.packageOverrides = in_pkgs : 
+    {
+	linuxPackages = in_pkgs.linuxPackages_latest; 
+    };
+
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.enableAllFirmware = true;
+  hardware.enableRedistributableFirmware = true;
+  programs.vim.defaultEditor = true;
+
+  networking.hostName = "jack";
+  networking.networkmanager.enable = true;
+
+
+  i18n = {
+    consoleKeyMap = "la-latin1";
+    defaultLocale = "es_CO.UTF-8";
+  };
+
+  time.timeZone = "America/Bogota";
+  services.timesyncd.enable = true;
+ 
+
+  # Activar Pulseaudio
+  sound.enable = true;
+  hardware.pulseaudio = {
+    enable = true;
+    package = pkgs.pulseaudioFull;
+  };
+
+
+  nixpkgs.config.allowUnfree = true;
+  virtualisation.libvirtd.enable = true;
+
+
+  fonts = {
+    enableDefaultFonts = true;
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fontconfig.ultimate.enable = true;
+    fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk 
+      noto-fonts-emoji
+      noto-fonts-extra
+      fira-code 
+      hack-font 
+      fira-mono 
+      corefonts  
+      roboto-mono
+      roboto 
+	dejavu_fonts
+    ];
+  };
+
+
+  environment.systemPackages = with pkgs; [
+	curl
+	wget
+	git
+	neofetch
+	micro
+	pavucontrol
+	networkmanager
+      nix-du 
+      common-updater-scripts
+      nixops
+      nix-review
+      nix-universal-prefetch	
+	google-chrome
+	unzip 
+	zip 
+	vim 
+  ];
+
+  security.sudo = {
+    enable = true; 
+    wheelNeedsPassword = true; 
+  };
+
+  users.user.jack = {
+    isNormalUser = true; 
+    uid = 1000;
+    group = "wheel";
+    createHome = true;
+    home = "/home/jack";
+  }
+
+  system.stateVersion = "22.05";
+
+}
+
---
+++ module.nix
@@ -0,0 +1,69 @@
+self: {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.programs.hyprland;
+in {
+  imports = [
+    (mkRemovedOptionModule ["programs" "hyprland" "extraPackages"] "extraPackages has been removed. Use environment.systemPackages instead.")
+  ];
+
+  options.programs.hyprland = {
+    enable = mkEnableOption ''
+      Hyprland, the dynamic tiling Wayland compositor that doesn't sacrifice on its looks.
+      You can manually launch Hyprland by executing "exec Hyprland" on a TTY.
+      A configuration file will be generated in ~/.config/hypr/hyprland.conf.
+      See <link xlink:href="https://github.com/vaxerski/Hyprland/wiki" /> for
+      more information.
+    '';
+
+    package = mkOption {
+      type = types.nullOr types.package;
+      default = self.packages.${pkgs.system}.default;
+      defaultText = literalExpression "<Hyprland flake>.packages.<system>.default";
+      example = literalExpression "<Hyprland flake>.packages.<system>.default.override { }";
+      description = ''
+        Hyprland package to use.
+      '';
+    };
+
+    recommendedEnvironment = mkOption {
+      type = types.bool;
+      default = true;
+      defaultText = literalExpression "true";
+      example = literalExpression "false";
+      description = ''
+        Whether to set the recommended environment variables.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment = {
+      systemPackages = lib.optional (cfg.package != null) cfg.package;
+
+      sessionVariables = mkIf cfg.recommendedEnvironment {
+        GDK_BACKEND = "wayland,x11";
+        _JAVA_AWT_WM_NONREPARENTING = "1";
+        NIXOS_OZONE_WL = "1";
+        XCURSOR_SIZE = "24";
+        XDG_SESSION_TYPE = "wayland";
+      };
+    };
+    fonts.enableDefaultFonts = mkDefault true;
+    hardware.opengl.enable = mkDefault true;
+    programs = {
+      dconf.enable = mkDefault true;
+      xwayland.enable = mkDefault true;
+    };
+    security.polkit.enable = true;
+    services.xserver.displayManager.sessionPackages = lib.optional (cfg.package != null) cfg.package;
+    xdg.portal = {
+      enable = mkDefault true;
+      extraPortals = [pkgs.xdg-desktop-portal-wlr];
+    };
+  };
+}
